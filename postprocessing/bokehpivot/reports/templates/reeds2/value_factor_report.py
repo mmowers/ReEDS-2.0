4212fb9f51ff0fad7985389b8b2863d2f3580bfc
mmowers <Matthew.Mowers@nrel.gov>
2025-02-16 17:05:43 -0500

    Update value factor report

--- postprocessing/bokehpivot/reports/templates/reeds2/value_factor_report.py
+++ postprocessing/bokehpivot/reports/templates/reeds2/value_factor_report.py
@@ -6,12 +6,11 @@ in reeds2.py for a description of each preset.
 storage_techs = ['pumped-hydro','pumped-hydro-flex','battery', 'battery_2', 'battery_4', 'battery_6', 'battery_8', 'battery_10']
 
 static_presets = [
-    {'name': 'Full Value New Techs', 'result': 'Value New Techs', 'download_full_source': True},
-    {'name': 'VF by Year', 'result': 'Value New Techs', 'preset': 'VF by Year'},
-    {'name': 'VF by Year Explode Scenario', 'result': 'Value New Techs', 'preset': 'VF by Year Explode Scenario'},
-    {'name': 'VF Energy by Year', 'result': 'Value New Techs', 'preset': 'VF Energy by Year'},
-    {'name': 'VF Firm Capacity by Year', 'result': 'Value New Techs', 'preset': 'VF Firm Capacity by Year'},
-    {'name': 'Gen Frac', 'result': 'Generation National (TWh)', 'preset': 'Stacked Bars Gen Frac', 'config':{'filter':{'tech':{'exclude':storage_techs}}}},
-    {'name': 'Cap Frac', 'result': 'Capacity National (GW)', 'preset': 'Stacked Bars Cap Frac'},
+    {'name': 'Full Value New Techs', 'sheet_name':'vf_full', 'result': 'Value New Techs', 'download_full_source': True},
+    {'name': 'VF by Year', 'sheet_name':'vf', 'result': 'Value New Techs', 'preset': 'VF by Year'},
+    {'name': 'VF Energy by Year', 'sheet_name':'vf_energy', 'result': 'Value New Techs', 'preset': 'VF Energy by Year'},
+    {'name': 'VF Firm Capacity by Year', 'sheet_name':'vf_firmcap', 'result': 'Value New Techs', 'preset': 'VF Firm Capacity by Year'},
+    {'name': 'Gen Frac', 'sheet_name':'gen_frac', 'result': 'Generation National (TWh)', 'preset': 'Stacked Bars Gen Frac', 'config':{'filter':{'tech':{'exclude':storage_techs}}}},
+    {'name': 'Cap Frac', 'sheet_name':'cap_frac', 'result': 'Capacity National (GW)', 'preset': 'Stacked Bars Cap Frac'},
     {'name': 'Bulk System Electricity Price ($/MWh)', 'sheet_name':'elec_price', 'result': 'Requirement Prices and Quantities National', 'preset': 'Bulk System Electricity Price ($/MWh)'},
 ]
